0075ca04c9e54e661f823af6c8287d75e86848eb
Jose <chaparro.jjr@gmail.com>
2020-08-25 13:55:05 -0700

    footer and navbar javascript added

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,8 +5,14 @@ let timesButton = document.querySelector('.fa-times');
 let burger = document.querySelector('.fa-bars');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 
+window.addEventListener('scroll', function () {
+  let windowPosition = window.scrollY > 50;
+  header.classList.toggle('active', windowPosition)
+})
+
+// Hamburger Toggler
 hamburgerMenu.addEventListener('click', function () {
   navList.classList.toggle('menu-open');
   timesButton.classList.toggle('show');
   burger.classList.toggle('hide');
-})
+});
